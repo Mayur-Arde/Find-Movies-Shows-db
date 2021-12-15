9d9b71f70f1c967d27bda794d597c61e14d02a3e
Mayur-Arde <mayurarde8@gmail.com>
2021-12-15 19:49:30 +0530

    first commit

--- src/context.js
+++ src/context.js
@@ -1,6 +1,6 @@
 import React, { useState, useContext, useEffect } from 'react';
 // make sure to use https
-export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`;
+export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=28ec5375 `;
 console.log(API_ENDPOINT);
 
 const AppContext = React.createContext();
